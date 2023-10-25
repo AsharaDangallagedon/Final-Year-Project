9416ff556c39eb2c44afe3b5bbd16a87ac2407f0
ashara <zkac422@live.rhul.ac.uk>
2023-10-25 02:48:42 +0100

    Added 5 html pages related to poetry to the folder and modified the code to take such html web pages as input and output the occurrence of each word in the poem

--- WordCount.java
+++ WordCount.java
@@ -6,23 +6,41 @@ import java.util.Scanner;
 
 public class WordCount {
    public static void main(String args[]) throws FileNotFoundException{
-    File file = new File("problem1.txt");
-    Scanner input = new Scanner(file);   
+    boolean div = false;
+    Scanner input;
     Map<String, Integer> wordCount = new HashMap<>();
-    while (input.hasNextLine()){
-        String line = input.nextLine();
-        String[] words = line.split("\s+");
-        for (String word: words){
-            String wordLowerCase = word.toLowerCase(); 
-            String wordUpperCase = word.toUpperCase();
-            if (wordCount.containsKey(wordLowerCase) || wordCount.containsKey(wordUpperCase)) {
-                wordCount.put(word, wordCount.get(word) + 1);
-            } else {
-                wordCount.put(word, 1);
+    String path = System.getProperty("user.dir");
+    File folder = new File(path);
+    File[] files = folder.listFiles();
+    for (File file : files) {
+        if (file.isFile() && file.getName().endsWith(".html")) {
+            input = new Scanner(file);
+            while (input.hasNextLine()){
+            String line = input.nextLine();
+            if (line.matches(".*<div class=\"post_description\">.*")) {
+                div = true;
             }
+            if (line.trim().equals("</div>")) {
+                div = false;
+            }
+            if (div) {
+                String[] words = line.split("\s+");
+                for (String word: words){
+                    word = word.replaceAll("<[^>]*>", "");
+                    word = word.replaceAll("[^a-zA-Z]+", "");
+                    word = word.replaceAll("^href\\S*", "");
+                    String wordLowerCase = word.toLowerCase();
+                    if (wordCount.containsKey(wordLowerCase)) {
+                        wordCount.put(wordLowerCase, wordCount.get(wordLowerCase) + 1);
+                    } else {
+                        wordCount.put(wordLowerCase, 1);}
+                    }
+                }
+            }
+            input.close();
         }
     }
-    input.close();
+    
     for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
         System.out.println(entry.getKey() + ": " + entry.getValue());
     }
