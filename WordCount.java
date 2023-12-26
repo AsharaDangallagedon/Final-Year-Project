961cf35cf119453897d9d5ca93a0d514c20defcb
ashara <zkac422@live.rhul.ac.uk>
2023-12-26 20:37:58 +0000

    Added the csv file to the repo

--- WordCount.java
+++ WordCount.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-
 import javax.naming.Context;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
