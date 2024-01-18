c09c907767f0b7888703c0b40397e0de4b16ec3c
Ashara <zkac422@live.rhul.ac.uk>
2024-01-18 23:14:22 +0000

    finally managed to calculate the mean for the column massExcessUncertainty

--- NuclearDecay.java
+++ NuclearDecay.java
@@ -1,41 +1,64 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
 import javax.naming.Context;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class NuclearDecay {
-
-    public static class MeanMapper extends Mapper<LongWritable, Text, Text, Text> {
-
+    public static class MeanMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+        private DoubleWritable massExcessUncertainty = new DoubleWritable();
+        @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
+            String[] columns = value.toString().split(",");
+            if (columns.length >= 11) {
+                String massExcessColumn = columns[10];
+                try {
+                    massExcessUncertainty.set(Double.parseDouble(massExcessColumn));
+                    context.write(new Text("Mean: "), massExcessUncertainty);
+                } catch (NumberFormatException e) {
+                    System.out.println("There was a problem with the column: " + "\\\"" + massExcessColumn + "\\\"");
+                }
+            }
         }
     }
 
-    public static class MeanReducer extends Reducer<Text, Text, Text, Text> {
-
+    public static class MeanReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+        @Override
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+            int count = 0;
+            double sum = 0.0;
+            for (DoubleWritable value : values) {
+                sum += value.get();
+                count++;
+            }
+            if (count > 0) {
+                double mean = sum / count;
+                context.write(key, new DoubleWritable(mean));            
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "statistical calculation");
         job.setJarByClass(NuclearDecay.class);
-        job.setInputFormatClass(CSVInputFormat.class); 
+        job.setInputFormatClass(TextInputFormat.class); 
         job.setMapperClass(MeanMapper.class);
         job.setCombinerClass(MeanReducer.class);
         job.setReducerClass(MeanReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class); 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+    }    
 }
