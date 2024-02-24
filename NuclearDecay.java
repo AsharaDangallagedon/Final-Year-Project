3ccbce8e6936e9fc8c22739d7334a9c6ccd47d1b
Ashara <zkac422@live.rhul.ac.uk>
2024-02-24 23:58:25 +0000

    managed to integrate the mnimum, maximum and mean calculations all under one single java file

--- NuclearDecay.java
+++ NuclearDecay.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import javax.naming.Context;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -14,51 +13,71 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class NuclearDecay {
-    public static class MeanMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
-        private DoubleWritable massExcessUncertainty = new DoubleWritable();  
+    public static class NuclearMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+        private DoubleWritable massExcess = new DoubleWritable();
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String[] columns = value.toString().split(",");
             if (columns.length >= 11) {
                 String massExcessColumn = columns[10];
                 try {
-                    massExcessUncertainty.set(Double.parseDouble(massExcessColumn));
-                    context.write(new Text("Mean: "), massExcessUncertainty);
+                    double massExcessValue = Double.parseDouble(massExcessColumn); 
+                    context.write(new Text("Mean: "), new DoubleWritable(massExcessValue));            
+                    context.write(new Text("Minimum: "), new DoubleWritable(massExcessValue));             
+                    context.write(new Text("Maximum: "), new DoubleWritable(massExcessValue));
                 } catch (NumberFormatException e) {
                     System.out.println("There was a problem with the value: " + "\\\"" + massExcessColumn + "\\\"");
                 }
             }
         }
     }
-
-    public static class MeanReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+    
+    
+    public static class NuclearReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+        private double minimumValue = 99.0;
+        private double maximumValue;
+        private double sum = 0.0;
+        private int count = 0;
         @Override
         public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-            int count = 0;
-            double sum = 0.0;
             for (DoubleWritable value : values) {
-                sum += value.get();
-                count++;
-            }
-            if (count > 0) {
-                double mean = sum / count;
-                context.write(key, new DoubleWritable(mean));            
+                double currentValue = value.get();  
+                if (key.toString().equals("Minimum: ") && currentValue < minimumValue) {
+                    minimumValue = currentValue;
+                }
+                if (key.toString().equals("Maximum: ") && currentValue > maximumValue) {
+                    maximumValue = currentValue;
+                }
+                if (key.toString().equals("Mean: ")) {
+                    sum += currentValue;
+                    count++;
+                }
             }
         }
+        @Override
+        public void cleanup(Context context) throws IOException, InterruptedException {       
+            if (count > 0) {
+                double mean = sum / count;
+                context.write(new Text("Mean: "), new DoubleWritable(mean));
+            }
+            context.write(new Text("Minimum: "), new DoubleWritable(minimumValue));
+            context.write(new Text("Maximum: "), new DoubleWritable(maximumValue));
+        }
     }
+    
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "statistical calculation");
-        job.setJarByClass(NuclearDecay.class);
-        job.setInputFormatClass(TextInputFormat.class); 
-        job.setMapperClass(MeanMapper.class);
-        job.setCombinerClass(MeanReducer.class);
-        job.setReducerClass(MeanReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(DoubleWritable.class); 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1); 
+        Job combinedJob = Job.getInstance(conf, "combined calculation");
+        combinedJob.setJarByClass(NuclearDecay.class);
+        combinedJob.setInputFormatClass(TextInputFormat.class); 
+        combinedJob.setMapperClass(NuclearMapper.class);
+        combinedJob.setCombinerClass(NuclearReducer.class);
+        combinedJob.setReducerClass(NuclearReducer.class);
+        combinedJob.setOutputKeyClass(Text.class);
+        combinedJob.setOutputValueClass(DoubleWritable.class); 
+        FileInputFormat.addInputPath(combinedJob, new Path(args[0]));
+        FileOutputFormat.setOutputPath(combinedJob, new Path(args[1]));
+        int combinedSuccess = combinedJob.waitForCompletion(true) ? 0 : 1;
     }       
 }
