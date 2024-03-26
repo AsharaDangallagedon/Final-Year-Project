0fb49aae68f717374eee59170e4078c4dfcb0846
Ashara <zkac422@live.rhul.ac.uk>
2024-03-26 00:09:45 +0000

    made slight changes to FrequencyDistribution.png and its python script such that testing can be done in an easier manner

--- FrequencyDistribution.py
+++ FrequencyDistribution.py
@@ -3,7 +3,7 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 data = {}
-with open("part-r-00000", "r") as file:
+with open("NuclearDecayDistributionOutput/part-r-00000", "r") as file:
     for line in file:
         line = line.strip()
         if "\t" in line:
@@ -16,10 +16,10 @@ for i in range(3, 26, 2):
 x_bar = [(range(3, 26, 2)[i] + range(3, 26, 2)[i+1]) / 2 for i in range(len(range(3, 26, 2)) - 1)]
 bar_values = [frequency_distribution[x] for x in range(3, 26, 2)[:-1]]
 
-plt.figure(figsize=(7, 5)) 
+plt.figure(figsize=(8, 6)) 
 plt.bar(x_bar, bar_values, width=1.2, color= "#2a2a2a")
 plt.xticks(range(3, 26, 2))  
-plt.xlabel("Mass Excess Uncertainty")
+plt.xlabel("Mass Excess Uncertainty (in atomic mass units u)")
 plt.ylabel("Cumulative Frequency")
-plt.title("Frequency Distribution")
+plt.title("Frequency Distribution for the Mass Excess Uncertainty of Isotopes of Elements")
 plt.savefig("frequency_distribution.png")
